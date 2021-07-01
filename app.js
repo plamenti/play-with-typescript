f34d6836c936a8abb9b1464d945e0749adbe0060
Plamen Kostadinov <kostapla@starsgroup.com>
2021-07-01 23:41:37 +0300

    Functions as type and with callback as parameter

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ function printResult(num) {
 }
 printResult(add(5, 12));
 // Functions as type
-var combineValues;
+var combineValues; // same as declaring an Interface of function
 combineValues = add;
 console.log(combineValues(5, 4));
+// Function with callback as param
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(2, 2, function (param) {
+    console.log("Callback: " + param);
+});
+var user = {
+    f: function () { console.log("plamen"); }
+};
+user.f();
--- app.ts
+++ app.ts
@@ -9,7 +9,25 @@ function printResult(num: number): void {
 printResult(add(5, 12));
 
 // Functions as type
-let combineValues: (a: number, b: number) => number; // same as declarin an Interface of function
+let combineValues: (a: number, b: number) => number; // same as declaring an Interface of function
 combineValues = add;
 
 console.log(combineValues(5, 4));
+
+// Function with callback as param
+function addAndHandle(n1: number, n2: number, cb: (n: number) => void): void {
+    const result = n1 + n2;
+    cb(result);
+}
+
+addAndHandle(2, 2, (param) => {
+    console.log("Callback: " + param);
+})
+
+const user: {
+    f: () => void;
+} = {
+    f: () => {console.log("plamen")}
+}
+
+user.f();
